bef03d4f4de775baefa6c02fa89ba2165d833e4a
Quentin Durant <quentin.durant49@orange.fr>
2026-04-02 11:59:18 +0200

    start form

--- src/components/form.vue
+++ src/components/form.vue
@@ -7,31 +7,23 @@
 </template>
 
 <script setup>
+    import { ref } from 'vue'
     import First from './stepForm/first.vue'
     import Second from './stepForm/second.vue'
     import Third from './stepForm/third.vue'
 
     const steps = [
-        {
-            name: 'first',
-            component: First
-        },
-        {
-            name: 'second',
-            component: Second
-        },
-        {
-            name: 'third',
-            component: Third
-        }
+        { name: 'first',  component: First },
+        { name: 'second', component: Second },
+        { name: 'third',  component: Third },
     ]
 
     const currentStep = ref(0)
+
     const previousStep = () => {
-        step--;
+        if (currentStep.value > 0) currentStep.value--
     }
     const nextStep = () => {
-        step++;
+        if (currentStep.value < steps.length - 1) currentStep.value++
     }
-
-    
+</script>
